b5b9f3e490d2743bb5a3653a8ce598ee6d092ac3
Ran Solash <rans@iguazio.com>
2019-01-09 16:46:50 -0300

    use iptables instead of ipvs

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cp -rfp inventory/sample/* inventory/igz/
 ./gen_templates.py ${@}
 
 ansible-playbook -i inventory/igz/hosts.ini cluster.yml -b \
-    -e '{ kubectl_localhost: True }' -e '{ kubeconfig_localhost: True }'
+    -e '{ kubectl_localhost: True }' -e '{ kubeconfig_localhost: True }' -e kube_proxy_mode=iptables
 
 ansible-playbook -i  inventory/igz/hosts.ini clients.yml
