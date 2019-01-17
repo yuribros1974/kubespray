6e5b2f0b48dba3af0075dcd0d87bf54e8b798c98
Ran Solash <rans@iguazio.com>
2019-01-17 08:38:46 -0300

    run kubespray deploy in offline mode

--- run.sh
+++ run.sh
@@ -5,7 +5,12 @@ set -ex
 cp -rfp inventory/sample/* inventory/igz/
 ./gen_templates.py ${@}
 
+ansible-playbook -i inventory/igz/hosts.ini offline_cache.yml \
+    -e 'release_cache_dir=./releases' -e '{ skip_downloads: True }'
+
 ansible-playbook -i inventory/igz/hosts.ini cluster.yml -b \
-    -e '{ kubectl_localhost: True }' -e '{ kubeconfig_localhost: True }' -e kube_proxy_mode=iptables
+    -e '{ kubectl_localhost: True }' -e '{ kubeconfig_localhost: True }' \
+    -e '{ deploy_container_engine: False }' -e '{ skip_downloads: True }' \
+    -e kube_proxy_mode=iptables
 
 ansible-playbook -i  inventory/igz/hosts.ini clients.yml
