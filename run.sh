3db1b973b18020a17fc4a94ea6e0e5b8a2d422a3
Ran Solash <rans@iguazio.com>
2019-02-11 17:46:59 -0300

    reset before install

--- run.sh
+++ run.sh
@@ -8,6 +8,8 @@ cp -rfp inventory/sample/* inventory/igz/
 ansible-playbook -i inventory/igz/hosts.ini offline_cache.yml \
     -e 'release_cache_dir=./releases' -e '{ skip_downloads: True }'
 
+ansible-playbook -i inventory/igz/hosts.ini reset_igz.yml -b -e kube_proxy_mode=iptables
+
 ansible-playbook -i inventory/igz/hosts.ini cluster.yml -b \
     -e '{ kubectl_localhost: True }' -e '{ kubeconfig_localhost: True }' \
     -e '{ deploy_container_engine: False }' -e '{ skip_downloads: True }' \
