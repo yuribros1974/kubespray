5a9a1840005a2aa0c2bc5820426b8458f6901f94
rans <ransolash@gmail.com>
2020-05-23 17:34:19 +0300

    Update prepare_offline_version.sh

--- prepare_offline_version.sh
+++ prepare_offline_version.sh
@@ -4,7 +4,7 @@ set -ex
 
 password=${1}
 
-pipenv --two install -r requirements.txt
+pipenv --python 2.7.5 install -r requirements.txt
 pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml -b \
     -e 'local_release_dir=./releases' -e '{ download_container: False }' \
     -e '{ skip_downloads: False }' -e ansible_os_family=RedHat -e ansible_sudo_pass=${password}
