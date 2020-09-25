8b51e9089acc8d461a2e5010b49904f9f20ea478
Vladimir <vladimir.fess@gmail.com>
2020-09-25 11:58:09 +0300

    build: bump kubernetes to 17.9 (#41)

--- prepare_offline_version.sh
+++ prepare_offline_version.sh
@@ -7,4 +7,4 @@ password=${1}
 pipenv --python 2.7.5 install -r requirements.txt
 pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml -b \
     -e 'local_release_dir=./releases' -e '{ download_container: False }' \
-    -e '{ skip_downloads: False }' -e ansible_os_family=RedHat -e ansible_sudo_pass=${password}
+    -e '{ skip_downloads: False }' -e ansible_os_family=RedHat -e ansible_become_password=${password}
