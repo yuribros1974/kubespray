dbe4bedb721d1be058d737914bbb1a5957aa18eb
Gall Zvyagin <gkirok@gmail.com>
2021-11-02 15:27:36 +0200

    fix

--- prepare_offline_version.sh
+++ prepare_offline_version.sh
@@ -5,6 +5,6 @@ set -ex
 password=${1}
 
 pipenv --python 2.7.5 install -r requirements.txt
-pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml -b \
+pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml \
     -e 'local_release_dir=./releases' -e '{ download_container: False }' \
     -e '{ skip_downloads: False }' -e ansible_os_family=RedHat -e ansible_become_password=${password}
