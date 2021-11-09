72838882e66cd70534a2847dcd23d12d224c54c0
eliyahun <eliyahun@iguazio.com>
2021-11-09 19:42:46 +0200

    update1

--- prepare_offline_version.sh
+++ prepare_offline_version.sh
@@ -10,14 +10,22 @@ cat inventory/local/hosts.ini
 hostname -I
 
 whoami
-
-
+ls -ltrh inventory/local/
 password=${1}
-
-mkdir -p "${WORKSPACE}/releases"
 
 pipenv --python 2.7.5 install -r requirements.txt
 export ANSIBLE_HOST_KEY_CHECKING=False && \
   pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml \
-    -e "local_release_dir=${WORKSPACE}/releases" -e '{ download_container: False }' \
+    -e 'local_release_dir=./releases' -e '{ download_container: False }' \
     -e '{ skip_downloads: False }' -e ansible_os_family=RedHat
+
+#
+#password=${1}
+#
+#mkdir -p "${WORKSPACE}/releases"
+#
+#pipenv --python 2.7.5 install -r requirements.txt
+#export ANSIBLE_HOST_KEY_CHECKING=False && \
+#  pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml \
+#    -e "local_release_dir=${WORKSPACE}/releases" -e '{ download_container: False }' \
+#    -e '{ skip_downloads: False }' -e ansible_os_family=RedHat
