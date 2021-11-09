b6493c7b44285374bb9b5aef751bde16097b4e2c
eliyahun <eliyahun@iguazio.com>
2021-11-09 20:06:34 +0200

    update1

--- prepare_offline_version.sh
+++ prepare_offline_version.sh
@@ -15,7 +15,7 @@ password=${1}
 
 pipenv --python 2.7.5 install -r requirements.txt
 export ANSIBLE_HOST_KEY_CHECKING=False && \
-  pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml \
+  pipenv run ansible-playbook  --become -i inventory/local/hosts.ini offline_cache.yml \
     -e 'local_release_dir=./releases' -e '{ download_container: False }' \
     -e '{ skip_downloads: False }' -e ansible_os_family=RedHat
 
