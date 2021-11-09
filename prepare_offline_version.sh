1c8f6dd00c00ccb1e41cc81a9ecebb41792bd2a0
eliyahun <eliyahun@iguazio.com>
2021-11-09 17:50:11 +0200

    update1

--- prepare_offline_version.sh
+++ prepare_offline_version.sh
@@ -9,6 +9,11 @@ cat inventory/local/hosts.ini
 
 password=${1}
 
+
+mkdir -p releases
+
+chmod 777 releases
+
 pipenv --python 2.7.5 install -r requirements.txt
 export ANSIBLE_HOST_KEY_CHECKING=False && \
   pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml \
