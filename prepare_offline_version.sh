771312290e5050c596a6883ec206ccd8ef56bf38
eliyahun <eliyahun@iguazio.com>
2021-11-09 13:15:20 +0200

    update1

--- prepare_offline_version.sh
+++ prepare_offline_version.sh
@@ -4,8 +4,12 @@ set -ex
 
 password=${1}
 
+
+cp -rp inventory/local/hosts.ini kubespray/inventory/local/hosts.ini
+
+
 pipenv --python 2.7.5 install -r requirements.txt
 export ANSIBLE_HOST_KEY_CHECKING=False && \
-  pipenv run ansible-playbook -i inventory/local/hosts.ini offline_cache.yml \
+  pipenv run ansible-playbook -i kubespray/inventory/local/hosts.ini offline_cache.yml \
     -e 'local_release_dir=./releases' -e '{ download_container: False }' \
     -e '{ skip_downloads: False }' -e ansible_os_family=RedHat 
